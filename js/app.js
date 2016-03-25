e98d7091b86618316571c02f4f7b0acfc20bcc8e
OoogleBoogle <simattfield@gmail.com>
2016-03-24 17:25:41 -0700

    Image swaps implemented

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
+    // triggers sound
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+});
