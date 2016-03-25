2bcc8adc3dcb31b0dddd7cb3ab50cfc54f7f1451
OoogleBoogle <simattfield@gmail.com>
2016-03-24 17:43:31 -0700

    Ryu now HADOUKENS

--- js/app.js
+++ js/app.js
@@ -8,9 +8,16 @@ $(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '590px');
+      });
     // triggers sound
   })
   .mouseup(function() {
@@ -18,3 +25,9 @@ $(function() {
     $('.ryu-ready').show();
   });
 });
+
+function playHadouken() {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
