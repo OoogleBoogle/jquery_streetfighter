cb6667d3db225e0b76d9240f1b47468f64314bcb
OoogleBoogle <simattfield@gmail.com>
2016-03-24 20:43:05 -0700

    Sounds and intro logos added

--- js/app.js
+++ js/app.js
@@ -1,28 +1,46 @@
 $(function() {
+
+  var still = $('.ryu-still');
+  var ready = $('.ryu-ready');
+  var throwing = $('.ryu-throwing');
+  var hadouken = $('.hadouken');
+  var cool = $('.ryu-cool');
+  intro();
   $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
+    still.hide();
+    ready.show();
   })
   .mouseleave(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
+    ready.hide();
+    still.show();
   })
   .mousedown(function() {
     playHadouken();
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show().animate(
-      {'left': '1020px'},
-      500,
+    ready.hide();
+    throwing.show();
+    hadouken.finish().show().animate(
+      {'left': '100vw'},
+      700,
       function() {
         $(this).hide();
         $(this).css('left', '590px');
       });
-    // triggers sound
   })
   .mouseup(function() {
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').show();
+    throwing.hide();
+    ready.show();
+  });
+
+  $(document).keypress(function(event) {
+    if (event.which === 120) {
+      ready.hide();
+      still.hide();
+      cool.show();
+    };
+  })
+  .keyup(function() {
+    cool.hide();
+    ready.show();
   });
 });
 
@@ -30,4 +48,27 @@ function playHadouken() {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
+}
+
+function intro() {
+  var fadeTime = 800
+  glitterSound();
+  $('.sf-logo').fadeIn(fadeTime).delay(fadeTime).fadeOut(fadeTime, function() {
+    $('.jq-logo').fadeIn(fadeTime).delay(fadeTime).fadeOut(fadeTime, function() {
+      $('.instructions').fadeIn(fadeTime);
+      startMusic();
+    });
+  });
+}
+
+function glitterSound() {
+  $('#glitter')[0].volume = 0.4;
+  $('#glitter')[0].load();
+  $('#glitter')[0].play();
+}
+
+function startMusic() {
+  $('#start-music')[0].volume = 0.6;
+  $('#start-music')[0].load();
+  $('#start-music')[0].play();
 }
